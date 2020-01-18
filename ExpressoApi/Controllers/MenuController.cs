57f770c980cb6fd04b3963835d13ba042cfdd2e7
icadir <ismailcadir@gmail.com>
2020-01-18 21:13:34 +0300

    Coffe APi

---
+++ ExpressoApi/Controllers/MenuController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpressoApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpressoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        ExpressoDbContext _expressoDbContext;
+
+        public MenuController(ExpressoDbContext expressoDbContext)
+        {
+            _expressoDbContext = expressoDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var menus = _expressoDbContext.Menus.Include("SubMenus");
+            return Ok(menus);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMenu(int id)
+        {
+            var menus = _expressoDbContext.Menus.Include("SubMenus").FirstOrDefault(x => x.Id == id);
+
+            if (menus==null)
+            {
+                return NotFound();
+            }
+
+            return Ok(menus);
+        }
+    }
+}
