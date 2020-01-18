bd5fb6c2ad7b2c86ebdfacd107d2835d2ce832ac
icadir <ismailcadir@gmail.com>
2020-01-18 13:32:25 +0300

    Validation Add

--- QuotesApi/Models/Quote.cs
+++ QuotesApi/Models/Quote.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace QuotesApi.Models
 {
     public class Quote
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Title { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Author { get; set; }
+        [Required]
+        [StringLength(500)]
         public string Description { get; set; }
+        [Required]
         public string Type { get; set; }
+        [Required]
         public DateTime CreatedAt { get; set; }
     }
 }
